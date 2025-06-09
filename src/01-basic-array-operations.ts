7369bb063292bf83a5892956e7ac68623df4f2e8
Polina Tereschenko <polinatereschenko16@gmail.com>
2025-06-09 13:51:14 +0300

    done 01-basic-array-operations

--- src/01-basic-array-operations.ts
+++ src/01-basic-array-operations.ts
@@ -9,8 +9,22 @@
  * Output: [2, 4, 6, 8]
  */
 export function doubleArrayElements(numbers: number[]): number[] {
-    // TODO: Implement this function
-    return [];
+    // const doubleArray: number[] = [];
+
+    // for (const index in numbers) {
+    //     doubleArray.push(numbers[index] * 2);
+    // }
+
+    // return doubleArray;
+
+    // const doubleArray: number[]  = [];
+    // for (let i = 0; i < numbers.length; i++) {
+    //     doubleArray.push(numbers[i] * 2);
+    // }
+
+    // return doubleArray;
+
+    return numbers.map(num => num * 2);
 }
 
 /**
@@ -24,8 +38,25 @@ export function doubleArrayElements(numbers: number[]): number[] {
  * Output: [2, 4, 6]
  */
 export function filterEvenNumbers(numbers: number[]): number[] {
-    // TODO: Implement this function
-    return [];
+    // const EvenNumbers: number[] = [];
+    // for (const index in numbers) {
+    //     if (numbers[index] % 2 === 0) {
+    //         EvenNumbers.push(numbers[index]);
+    //     }
+    // }
+
+    // return EvenNumbers;
+
+    // const EvenNumbers: number[] = [];
+    // for (let i = 0; i < numbers.length; i++) {
+    //     if (numbers[i] % 2 === 0) {
+    //         EvenNumbers.push(numbers[i]);
+    //     }
+    // }
+
+    // return EvenNumbers;
+
+    return numbers.filter(num => num % 2 === 0);
 }
 
 /**
@@ -38,7 +69,20 @@ export function filterEvenNumbers(numbers: number[]): number[] {
  * Input: [1, 2, 3, 4]
  * Output: 10
  */
-export function sumArrayElements(numbers: number[]): number {
-    // TODO: Implement this function
-    return 0;
+export function sumArrayElements(numbers: number[]): number {    
+    // let sumArray  = 0;
+    // for (const index in numbers) {
+    //     sumArray += numbers[index];
+    // }
+
+    // return sumArray;
+
+    // let sumArray = 0;
+    // for (let i = 0; i < numbers.length; i++) {
+    //     sumArray += numbers[i];
+    // }
+
+    // return sumArray;
+
+    return numbers.reduce((i, num) => i + num, 0);
 } 
